b192669db0428b04a55fc6589bb03ba4e3e7590f
Lazaro Carvalho <lazaro.oliveiras@hotmail.com>
2020-02-27 15:24:55 -0300

    recurso de generos das series

--- src/server.js
+++ src/server.js
@@ -16,9 +16,11 @@ let allowCrossDomain = (req, res, next) => {
 
 const series = require('./routes/seriesRoutes');
 const auth = require('./routes/authRoutes');
+const generos = require('./routes/generosRoutes');
 
 app.use(allowCrossDomain);
 app.use('/auth', auth);
+app.use('/series/generos', generos)
 app.use(authMidd);
 app.use('/series', series);
 
